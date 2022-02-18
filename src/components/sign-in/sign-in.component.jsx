28efdc705c30dd8937378339e544afc8cb3842b1
rajkoKupinac <rajkokupinac@gmail.com>
2022-02-18 14:13:00 +0100

    Finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './sign-in.styles.scss'
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component{
     constructor(props){
@@ -12,9 +12,17 @@ class SignIn extends React.Component{
             password: ''
         }
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email:'', password:''})
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email:'', password:''})
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
     handleChange = event => {
         const {value, name} = event.target
@@ -29,8 +37,8 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" value={this.state.email} required handleChange={this.handleChange} label="Email"/>
-                    <FormInput name="password" value={this.state.password} required handleChange={this.handleChange} label="Password"/>
+                    <FormInput type="email" name="email" value={this.state.email} required handleChange={this.handleChange} label="Email"/>
+                    <FormInput type="password" name="password" value={this.state.password} required handleChange={this.handleChange} label="Password"/>
                     <div className="buttons">
                         <CustomButton children={"SIGN IN"} type="submit" onSubmit={this.handleSubmit}/>
                         <CustomButton isGoogleSignIn children={"SIGN IN WITH GOOGLE"} onClick={signInWithGoogle}/>
